02d7e2ac051ed0bd6dd7f0ae80a07bf65a102f13
Sten Turpin <sten@spectrocloud.com>
2023-04-11 16:47:16 -0500

    use palette application profile to deploy example app

--- jenkins-jobs/resources.tf
+++ jenkins-jobs/resources.tf
@@ -3,6 +3,48 @@ data "spectrocloud_cluster_group" "cg" {
   context = "project"
 }
 
+variable "example-app-image" {
+    type        = string
+    description = "The name of the container image to use for the virtual cluster in a single scenario"
+    default     = "ghcr.io/stenwt/vcluster-app:main"
+}
+
+data "spectrocloud_registry" "container_registry" {
+  name = "Public Repo"
+}
+
+data "spectrocloud_pack_simple" "container_pack" {
+  type         = "container"
+  name         = "container"
+  version      = "1.0.0"
+  registry_uid = data.spectrocloud_registry.container_registry.id
+}
+
+resource "spectrocloud_application_profile" "example-app" {
+  name        = "example"
+  description = "example application"
+  pack {
+    name = "example"
+    type = data.spectrocloud_pack_simple.container_pack.type
+    registry_uid = data.spectrocloud_registry.container_registry.id
+    source_app_tier = data.spectrocloud_pack_simple.container_pack.id
+    values = <<-EOT
+        pack:
+          namespace: "{{.spectro.system.appdeployment.tiername}}-ns"
+          releaseNameOverride: "{{.spectro.system.appdeployment.tiername}}"
+        containerService:
+            serviceName: "{{.spectro.system.appdeployment.tiername}}-svc"
+            registryUrl: ""
+            image: ${var.example-app-image}
+            access: public
+            ports:
+              - "80"
+            serviceType: LoadBalancer
+    EOT
+  }
+  tags = ["example-app"]
+}
+
 resource "spectrocloud_virtual_cluster" "cluster" {
   name = "virtual-cluster-jenkins-ci"
 
@@ -16,6 +58,22 @@ resource "spectrocloud_virtual_cluster" "cluster" {
     max_storage_in_gb = "0"
     min_storage_in_gb = "0"
   }
+
+  timeouts {
+    create = "10m"
+    update = "10m"
+  }
+}
+
+resource "spectrocloud_application" "example-app-deployment" {
+  name                    = "example"
+  application_profile_uid = spectrocloud_application_profile.example-app.id
+
+  config {
+    cluster_name = spectrocloud_virtual_cluster.cluster.name
+    cluster_uid  = spectrocloud_virtual_cluster.cluster.id
+  }
+  tags = ["example-app"]
 
   timeouts {
     create = "10m"
